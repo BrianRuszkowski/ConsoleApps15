46918ede4fba2e2a13118a6206d00e94a569a97d
BrianRuszkowski <72171622+BrianRuszkowski@users.noreply.github.com>
2021-04-20 12:17:16 +0100

    Finished

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -3,18 +3,35 @@
 namespace ConsoleAppProject.App01
 {
     /// <summary>
-    /// Please describe the main features of this App
+    /// This is my mesurement converter which you can can convert units from and to a 
+    /// certain mesurment and get the output and the calculation for the mesurement
     /// </summary>
     /// <author>
-    /// Student Name version 0.1
+    /// Brian Ruszkowski 21717237 version 1.0
     /// </author>   
+    
+    ///<summary>
+    ///This code is for the value of each of the mesurement from and to value
+    /// </summary>
     public class DistanceConverter
     {
-        public const int FEET_IN_MILES = 5280;
+        public const double FEET_IN_FEET = 1.0;
 
         public const double METRES_IN_MILES = 1609.34;
 
         public const double FEET_IN_METRES = 3.28084;
+
+        public const double FEET_IN_MILES = 5280;
+
+        public const double METRES_IN_METRES = 1.0;
+
+        public const double MILES_IN_MILES = 1.0;
+
+        public const double MILES_IN_FEET = 0.0;
+
+        public const double METRES_IN_FEET = 3.28084;
+
+        public const double MILES_IN_METRES = 0.0;
 
         public const string FEET = "Feet";
         public const string METRES = "Metres";
@@ -32,6 +49,10 @@ namespace ConsoleAppProject.App01
             toUnit = FEET;
         }
 
+        /// <summary>
+        /// Distance converter this code outputs the comment to select mesurment to and from 
+        /// and shows which mesurments you have selected it also calculates both mesurments selected
+        /// </summary>
         public void ConvertDistance()
         {
             OutputHeading();
@@ -48,18 +69,54 @@ namespace ConsoleAppProject.App01
             OutputDistance();
         }
 
+        /// <summary>
+        /// Distance calculator this code allows the user to select the to and from unit
+        /// and giving them the convertion calculation 
+        /// </summary>
         private void CalculateDistance()
         {
-            if(fromUnit == MILES && toUnit == FEET)
+            if(fromUnit == FEET && toUnit == FEET)
             {
-                toDistance = fromDistance * FEET_IN_MILES;
+                toDistance = fromDistance * FEET_IN_FEET;
             }
             else if(fromUnit == FEET && toUnit == MILES)
             {
                 toDistance = fromDistance / FEET_IN_MILES;
             }
+            else if(fromUnit == FEET && toUnit == METRES)
+            {
+                toDistance = fromDistance / FEET_IN_METRES;
+            }
+            else if (fromUnit == MILES && toUnit == FEET)
+            {
+                toDistance = fromDistance / MILES_IN_FEET;
+            }
+            else if (fromUnit == MILES && toUnit == MILES)
+            {
+                toDistance = fromDistance / MILES_IN_MILES;
+            }
+            else if (fromUnit == MILES && toUnit == METRES)
+            {
+                toDistance = fromDistance / MILES_IN_METRES;
+            }
+            else if (fromUnit == METRES && toUnit == FEET)
+            {
+                toDistance = fromDistance / METRES_IN_FEET;
+            }
+            else if (fromUnit == METRES && toUnit == METRES)
+            {
+                toDistance = fromDistance / METRES_IN_METRES;
+            }
+            else if (fromUnit == METRES && toUnit == MILES)
+            {
+                toDistance = fromDistance / METRES_IN_MILES;
+            }
         }
 
+        /// <summary>
+        /// This code displays which choice the user has selected the to and from measurements
+        /// and shows both units chosen
+        /// </summary>
         private string SelectUnit(string prompt)
         {
             string choice = DisplayChoices(prompt);
@@ -69,6 +126,10 @@ namespace ConsoleAppProject.App01
             return unit;
         }
 
+        /// <summary>
+        /// This code is for the choices which allow the user to input which 
+        /// conversion of measurement they want to choose to and from
+        /// </summary>
         private static string ExecuteChoice(string choice)
         {
             if (choice.Equals("1"))
@@ -87,6 +148,10 @@ namespace ConsoleAppProject.App01
             return null;
         }
 
+
+        /// <summary>
+        /// This code displays options of conversion for the user to unit and from unit
+        /// </summary>
         private static string DisplayChoices(string prompt)
         {
             Console.WriteLine();
@@ -100,6 +165,9 @@ namespace ConsoleAppProject.App01
             return choice;
         }
 
+        /// <summary>
+        /// This code is for the input of the measurement
+        /// </summary>
         private double InputDistance(string prompt)
         {
             Console.Write(prompt);
@@ -107,13 +175,18 @@ namespace ConsoleAppProject.App01
             return Convert.ToDouble(value);
         }
 
-
+        /// <summary>
+        /// This code is for the output of the measurement 
+        /// </summary>
         private void OutputDistance()
         {
             Console.WriteLine($"\n {fromDistance} {fromUnit}" + 
                 $" is {toDistance} {toUnit}!\n");
         }
 
+        /// <summary>
+        /// This code displays what the app is and the authors name
+        /// </summary>
         private void OutputHeading()
         {
             Console.WriteLine("\n--------------------------------------------");
